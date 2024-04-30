85c9538d98f9417e19eae33c0dbfb6559ede596e
Wonders Victor <wondersprince1@gmail.com>
2024-04-30 10:49:37 +0100

    Fixes for 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (!set || !(set instanceof Set) || !startString || typeof startString !== 'string') {
+    return '';
+  }
   const finalString = [];
   set.forEach((element) => {
     if (startString !== '' && element.startsWith(startString)) {
