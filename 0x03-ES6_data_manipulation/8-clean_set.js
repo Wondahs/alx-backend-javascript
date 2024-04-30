9fef50568be1cc42219042b96569950ed489371b
Wonders Victor <wondersprince1@gmail.com>
2024-04-30 11:01:33 +0100

    Final update for 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
   const allStrings = Array.from(set).every((element) => typeof element === 'string');
 
-  if (!set || !(set instanceof Set) || !allStrings || !startString || typeof startString !== 'string') {
+  if (!set || !(set instanceof Set) || !allStrings || !startString || startString === '' || typeof startString !== 'string') {
     return '';
   }
   const finalString = [];
   set.forEach((element) => {
-    if (startString !== '' && element.startsWith(startString)) {
+    if (element.startsWith(startString)) {
       const subString = element.replace(startString, '');
       finalString.push(subString);
     }
