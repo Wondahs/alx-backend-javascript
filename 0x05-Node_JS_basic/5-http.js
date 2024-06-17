93a3889d1bf0ad63bebd2c0ef79b91fb93ef172d
Wonders Victor <wondersprince1@gmail.com>
2024-06-17 12:21:48 +0000

    Express!

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,6 +9,7 @@ function parseData(data) {
     const content = contents.filter(line => line !== "");
     const cs = [];
     const swe = [];
+    const result = [];
 
     for (let line of content) {
       const column = line.split(',');
@@ -19,9 +20,10 @@ function parseData(data) {
       }
     }
 
-    console.log(`Number of students: ${content.length}`);
-    console.log(`Number of students in CS: ${cs.length}. ${cs.join(', ')}`);
-    console.log(`Number of students in SWE: ${swe.length  }. ${swe.join(', ')}`);
+    result.push(`Number of students: ${content.length}`);
+    result.push(`Number of students in CS: ${cs.length}. ${cs.join(', ')}`);
+    result.push(`Number of students in SWE: ${swe.length  }. ${swe.join(', ')}`);
+    return result.join('\n');
   } catch (error) {
     throw new Error('Cannot load the database');
   }
@@ -38,4 +40,37 @@ function countStudents(path) {
   })
 }
 
+// A simple http server
+const filepath = process.argv.length > 2 ? process.argv[2] : "q"
+const http = require('http');
+const port = 1245;
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-type', 'text/plain');
 
+  switch(req.url) {
+    case '/':
+      res.statusCode = 200;
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.statusCode = 200;
+      const result = ["This is the list of our students"];
+      let students;
+      if (filepath !== "") {
+        students = await countStudents(filepath);
+      } else {
+        students = "";
+      }
+      result.push(students);
+      res.end(result.join('\n'));
+      break;
+    default:
+      break;
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+})
+
+module.exports = app;
