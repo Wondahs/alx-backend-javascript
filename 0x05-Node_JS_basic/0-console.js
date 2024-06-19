4288d3fd0beeafe26cd59296fc3b3c943bfe760d
Wonders Victor <Wondersprince1@gmail.com>
2024-06-19 09:43:59 +0100

    Fixes

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // displayMessage that prints in STDOUT the string argument.
 function displayMessage(message) {
-  process.stdout.write(message + '\n')
+  console.log(message);
 }
 
 module.exports = displayMessage;
