1ba4bbbde9cbbf685a8f10c66752af704dfedda8
Wonders Victor <Wondersprince1@gmail.com>
2024-06-20 07:34:16 +0100

    Switch to require instead of import

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,14 +2,14 @@
 /* eslint-disable jest/valid-expect */
 /* eslint-disable import/no-extraneous-dependencies */
 // 3-payment.test.js
-import { spy } from 'sinon';
-import expect from 'chai';
-import Utils from './utils';
-import sendPaymentRequestToApi from './3-payment';
+const sinon = require('sinon');
+const expect = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('should call Utils.calculateNumber', () => {
-    const main = spy(Utils);
+    const main = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
     expect(main.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
