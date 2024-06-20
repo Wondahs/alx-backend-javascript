f314757af9430721cf7412a587db9f88ffd696c2
Wonders Victor <wondersprince1@gmail.com>
2024-06-20 13:05:45 +0000

    Task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,10 +1,12 @@
 // 6-payment_token.test.js
 const assert = require('assert');
 const getPaymentTokenFromAPI = require('./6-payment_token');
+const {get} = require('http');
 
 describe('getPaymentTokenFromAPI', () => {
-    it('test the result of getPaymentTokenFromAPI(true)', () => {
-        expected_result = 
-        assert.strictEqual(getPaymentTokenFromAPI(true), )
-    })
-})
+  it('test the result of getPaymentTokenFromAPI(true)', async () => {
+    expected_result = { data: 'Successful response from the API' }
+    result = await getPaymentTokenFromAPI(true);
+    assert.deepStrictEqual(result, expected_result);
+  });
+});
