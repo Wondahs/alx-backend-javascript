d827b8c79b5be9ce2622f5e34c71f65563aeb2b8
Wonders Victor <Wondersprince1@gmail.com>
2024-06-20 12:03:04 +0100

    working

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -5,7 +5,7 @@
 const sinon = require('sinon');
 const assert = require('assert');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./4-payment');
+const sendPaymentRequestToApi = require('./5-payment');
 
 describe('test sendPaymentRequestToApi', () => {
   let stub;
@@ -22,17 +22,30 @@ describe('test sendPaymentRequestToApi', () => {
     stub.restore();
     consoleSpy.restore();
   });
-  it('should call Utils.calculateNumber and log 10', () => {
-    // Configure stub to return 10
-    stub.returns(10);
-
+  it('should call Utils.calculateNumber and log 120', () => {
+    // Set stub return
+    stub.returns(120);
     // Call function being tested
     sendPaymentRequestToApi(100, 20);
 
     // Verify that Utils.calculateNumber was called with the appropriate values
     assert.strictEqual(stub.calledWith('SUM', 100, 20), true);
 
-    // verify that right output was logged
-    assert.strictEqual(consoleSpy.calledWith('The total is: 10'), true);
+    // Verify that right output was logged
+    assert.strictEqual(consoleSpy.calledWith('The total is: 120'), true);
+  });
+
+  it('should call Utils.calculateNumber and log 20', () => {
+    // Set stub return
+    stub.returns(20);
+  
+    // Call function being tested
+    sendPaymentRequestToApi(10, 10);
+
+    // Verify that Utils.calculateNumber was called with the appropriate values
+    assert.strictEqual(stub.calledWith('SUM', 10, 10), true);
+
+    // Verify that right output was logged
+    assert.strictEqual(consoleSpy.calledWith('The total is: 20'), true);
   });
 });
